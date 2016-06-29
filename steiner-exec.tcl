668277ba8941ec07bac93ff68ebde834fb73de71
rvzm <rvxm@outlook.com>
2016-06-29 15:12:12 -0800

    no message

--- steiner-exec.tcl
+++ steiner-exec.tcl
@@ -12,34 +12,32 @@ if {[catch {source scripts/steiner-settings.tcl} err]} {
 }
 
 namespace eval steiner {
-   namespace eval exec {
-	# Protected functions
-	set flag $steiner::settings::exec::flag
-	if {$steiner::settings::exec::enablepublic} { foreach cmd {date sysinfo uname uptime} { bind pub ${flag}|${flag} ${steiner::settings::prefix}$cmd steiner::exec::public } }
-	# Public functions
-	if {$steiner::settings::exec::enablepublic} { foreach cmd {stats times} { bind pub -|- ${steiner::settings::prefix}$cmd steiner::exec::public } }
-	proc public {nick uhost hand chan arg} {
-		if {[string tolower [lindex [split $arg] 0]] == "-p"} { set targ "$nick" } else { set targ "$chan" }
-		set cmd [string range [lindex $::lastbind 0] 1 end]
-		puthelp "PRIVMSG $targ :[execute $cmd]"
-	}
+	namespace eval exec {
+		# Protected functions
+		set flag $steiner::settings::exec::flag
+		if {$steiner::settings::exec::enablepublic} { foreach cmd {date sysinfo uname uptime} { bind pub ${flag}|${flag} ${steiner::settings::prefix}$cmd steiner::exec::public } }
+		# Public functions
+		if {$steiner::settings::exec::enablepublic} { foreach cmd {stats times} { bind pub -|- ${steiner::settings::prefix}$cmd steiner::exec::public } }
+		proc public {nick uhost hand chan arg} {
+			if {[string tolower [lindex [split $arg] 0]] == "-p"} { set targ "$nick" } else { set targ "$chan" }
+			set cmd [string range [lindex $::lastbind 0] 1 end]
+			puthelp "PRIVMSG $targ :[execute $cmd]"
+		}
 
-	proc execute { cmd } {
-		if {$cmd == "sysinfo"}	{ set cmd "/usr/local/bin/sysinfo" }
-		if {$cmd == "uname"}	{ set cmd "uname -a" }
-
-		if {$cmd == "date"}     { return [clock format [clock seconds] -format {%Y-%m-%d %T %Z}] }
-		if {$cmd == "stats"}	{ return "Channel stats can be found at http://scottsteiner.github.io" }
-		if {$cmd == "times"}	{
-			foreach city [dict keys $steiner::settings::exec::times] {
-				set timezone [dict get $steiner::settings::exec::times $city]
-				lappend info "$city [clock format [clock seconds] -format {%I:%M %p %Z} -timezone $timezone]"
+		proc execute { cmd } {
+			if {$cmd == "sysinfo"}	{ set cmd "/usr/local/bin/sysinfo" }
+			if {$cmd == "uname"}	{ set cmd "uname -a" }
+			if {$cmd == "date"}     { return [clock format [clock seconds] -format {%Y-%m-%d %T %Z}] }
+			if {$cmd == "times"}	{
+				foreach city [dict keys $steiner::settings::exec::times] {
+					set timezone [dict get $steiner::settings::exec::times $city]
+					lappend info "$city [clock format [clock seconds] -format {%I:%M %p %Z} -timezone $timezone]"
+				}
+				set output [join $info " | "]
+				return "$output"
 			}
-			set output [join $info " | "]
+			catch { eval exec $cmd } output
 			return "$output"
 		}
-		catch { eval exec $cmd } output
-		return "$output"
 	}
-   }
 }
